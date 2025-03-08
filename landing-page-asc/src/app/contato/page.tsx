150a633614c1a4831994df9df16407028c388114
Daniel Nogueira <danielsilvangr@gmail.com>
2025-03-08 19:37:08 -0300

    :sparkles: feat: Cria o Menu e Hero

---
+++ landing-page-asc/src/app/contato/page.tsx
@@ -0,0 +1,13 @@
+const Contato = () => {
+  return (
+    <>
+      <h1>Contato akjskl</h1>
+    </>
+  );
+};
+
+export default Contato;
+
+// É uma página
+// Nomeio como page.tsx
+// E preciso usar default
